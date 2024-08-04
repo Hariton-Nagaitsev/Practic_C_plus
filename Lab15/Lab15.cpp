0a9ddf87f2d1954cf9ebc4866b8eca9cf57a6663
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2024-08-04 19:39:40 +0300

    Add files via upload

---
+++ Lab15/Lab15.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+struct StudentGrade {
+    std::string name;
+    char grade;
+};
+
+StudentGrade Input() {
+    StudentGrade stud;
+    std::cout << "Введите имя студента: ";
+    std::cin >> stud.name;
+    std::cout << "Введите оценку: ";
+    std::cin >> stud.grade;
+    return stud;
+}
+
+void Show(const StudentGrade& stud) {
+    std::cout << stud.name << " - " << stud.grade << "\n";
+}
+
+int main() {
+    system("chcp 1251");
+
+    std::map<std::string, char> students;
+    bool flag = true;
+
+    while (flag) {
+        StudentGrade stud = Input();
+
+        if (students.count(stud.name) == 0) {
+            students[stud.name] = stud.grade;
+        }
+        else {
+            std::cout << "У студента уже стоит оценка." << std::endl;
+        }
+
+        std::cout << "Введите 1, чтобы продолжить ввод оценок, или 0, чтобы прекратить: ";
+        std::cin >> flag;
+    }
+
+    for (const auto& element : students) {
+        std::cout << "Фамилия: " << element.first << "\t\t" << "Оценка - " << element.second << std::endl;
+    }
+
+    return 0;
+}
