8707ec0581e97d39cd0186e243f000726d8aa1eb
Hariton-Nagaitsev <148050661+Hariton-Nagaitsev@users.noreply.github.com>
2024-08-04 19:39:28 +0300

    Add files via upload

---
+++ Lab14/Lab14_02/Lab14_02.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+
+template <class T>
+void Print(const T& container, const std::string& separator) {
+    auto it = container.begin();
+    if (it != container.end()) {
+        std::cout << *it;
+        ++it;
+    }
+    for (; it != container.end(); ++it) {
+        std::cout << separator << *it;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> data1 = { 1, 2, 3 };
+    Print(data1, ", ");
+
+    std::vector<char> data2 = { 'a', 'e', 't', 'r', '7' };
+    Print(data2, ", ");
+
+    std::vector<double> data3 = { 1.06, 2.89, 3.00, 45.5, -35 };
+    Print(data3, ", ");
+
+    std::list<std::string> data4 = { "Hello", "world", "!" };
+    Print(data4, " ");
+
+    std::set<int> data5 = { 10, 20, 30, 40, 50 };
+    Print(data5, " - ");
+
+    return 0;
+}
